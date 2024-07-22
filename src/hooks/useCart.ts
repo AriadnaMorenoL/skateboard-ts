4bac428b98272792c1c233ff549b4b5aa44183e7
AriadnaMoreno <135199503+AriadnaMorenoL@users.noreply.github.com>
2024-07-21 21:28:20 -0600

    - Replaced custom hook with useReducer to improve state management.

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -1,5 +1,6 @@
-import { useState, useEffect,  } from "react";
-import type { CartItem, } from '../types/types'
+import { useState, useEffect, useMemo } from "react";
+import { db } from "../data/db";
+import type {Board, CartItem, } from '../types/types'
 
 export const useCart = () => {
   const initialState = () : CartItem[] => {
@@ -7,69 +8,81 @@ export const useCart = () => {
     return localStorageCart ? JSON.parse(localStorageCart) : [];
   };
 
-  const [cart  , ] = useState(initialState);
+  const [data] = useState(db);
+  const [cart  , setCart] = useState(initialState);
 
-  // const MAX_ITEMS = 5;
-  // const MIN_ITEMS = 1;
+  const MAX_ITEMS = 5;
+  const MIN_ITEMS = 1;
 
   useEffect(() => {
     localStorage.setItem("cart", JSON.stringify(cart));
   }, [cart]);
 
-  // function addToCart(item : Board) {
-  //   const itemExist = cart.findIndex((board) => board.id === item.id);
-  //   if (itemExist >= 0) {
-  //     if (cart[itemExist].quantity >= MAX_ITEMS) return;
-  //     const updateCart = [...cart];
-  //     updateCart[itemExist].quantity += 1;
-  //     setCart(updateCart);
-  //   } else {
-  //     const newItem : CartItem = {...item, quantity: 1}
-  //     setCart((cart) => [...cart, newItem]);
-  //   }
-  // }
-  // function removeFromCart(id : Board['id']) {
-  //   const updateCart = cart.filter((board) => board.id !== id);
-  //   setCart(updateCart);
-  // }
+  function addToCart(item : Board) {
+    const itemExist = cart.findIndex((board) => board.id === item.id);
+    if (itemExist >= 0) {
+      if (cart[itemExist].quantity >= MAX_ITEMS) return;
+      const updateCart = [...cart];
+      updateCart[itemExist].quantity += 1;
+      setCart(updateCart);
+    } else {
+      const newItem : CartItem = {...item, quantity: 1}
+      setCart((cart) => [...cart, newItem]);
+    }
+  }
+  function removeFromCart(id : Board['id']) {
+    const updateCart = cart.filter((board) => board.id !== id);
+    setCart(updateCart);
+  }
 
   //  const removeFromCart = (id) =>{
   //     setCart(prevCart => prevCart.filter(guitar => guitar.id !== id))
   //   }
 
-  // function decreaseItem(id : Board['id']) {
-  //   const updateQuantity = cart.map((item) => {
-  //     if (item.id === id && item.quantity > MIN_ITEMS) {
-  //       return {
-  //         ...item,
-  //         quantity: item.quantity - 1,
-  //       };
-  //     }
-  //     return item;
-  //   });
-  //   setCart(updateQuantity);
-  // }
-  // function increaseItem(id : Board['id']) {
-  //   const updateQuantity = cart.map((item) => {
-  //     if (item.id === id && item.quantity < MAX_ITEMS) {
-  //       return {
-  //         ...item,
-  //         quantity: item.quantity + 1,
-  //       };
-  //     }
-  //     return item;
-  //   });
-  //   setCart(updateQuantity);
-  // }
+  function decreaseItem(id : Board['id']) {
+    const updateQuantity = cart.map((item) => {
+      if (item.id === id && item.quantity > MIN_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity - 1,
+        };
+      }
+      return item;
+    });
+    setCart(updateQuantity);
+  }
+  function increaseItem(id : Board['id']) {
+    const updateQuantity = cart.map((item) => {
+      if (item.id === id && item.quantity < MAX_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity + 1,
+        };
+      }
+      return item;
+    });
+    setCart(updateQuantity);
+  }
 
-  // function clearCart() {
-  //   setCart([]);
-  // }
+  function clearCart() {
+    setCart([]);
+  }
 
-  
+  const isEmpty = useMemo(() => cart.length === 0, [cart]);
+  const cartTotal = useMemo(
+    () => cart.reduce((total, item) => total + item.quantity * item.price, 0),
+    [cart]
+  );
 
   return {
+    data,
     cart,
-
+    addToCart,
+    removeFromCart,
+    increaseItem,
+    decreaseItem,
+    clearCart,
+    isEmpty,
+    cartTotal,
   };
 };
